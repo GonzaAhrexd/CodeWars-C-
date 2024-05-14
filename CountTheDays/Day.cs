6999bbe962a60f1fd406f7054918fda17cebcf01
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-05-14 19:27:30 -0300

    New DateTime challenge

---
+++ CountTheDays/Day.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountTheDays
+{
+    public class Day{
+        public string countDays(DateTime d)
+        {
+            if(d.ToLongDateString().Equals(DateTime.Now.ToLongDateString())){
+                return "Today is the day!";
+            }
+            else if(d < DateTime.Now){
+                return "The day is in the past!";
+            }
+            else{
+                return (d - DateTime.Now).Days + " days";
+            }
+        }
+    }
+}
---
+++ CountTheDays/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CountTheDays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day day = new Day();
+
+            Console.WriteLine(day.countDays(new DateTime(2016, 12, 2)));
+            Console.WriteLine(day.countDays(DateTime.Now));
+            Console.WriteLine(day.countDays(new DateTime(2025, 12, 2)));
+
+        }
+    }
+}
